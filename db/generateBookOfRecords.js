StuBlad/neohabitat

commit 1bd92fc7f211a47eb62fc74ef923ca5b5f0d725d

Modernizes Neohabitat to work with modern Mongo/JS/Java

db/generateBookOfRecords.js
@@ -9,7 +9,7 @@
  * By: Randy Farmer April 2017
  */
 
-const	File		= require('fs');
+const	File		= require('fs').promises;
 const	Trace 		= require('winston');
 const	MongoClient	= require('mongodb').MongoClient;
 const	Assert 		= require('assert');
@@ -32,6 +32,8 @@ const 	Argv		= require('yargs')
 .option('mongo', { alias: 'm', default: Defaults.mongo, describe: 'Mongodb server URL'})
 .option('book',	 { alias: 'b', default: Defaults.book,  describe: 'JSON output file for The Book of Records.'})
 .argv;
+
+Trace.level = Argv.trace;
 
 const HS$lifetime          		=   1;
 const HS$max_lifetime      		=   2;
@@ -191,39 +193,39 @@ function generateRecords(userRecords) {
 
 
 
-function processUserStats(users)  {
+const processUserStats = async (users) => {
 	var userRecords = {};
-	for (i in users) {
+	users.forEach((user) => {
 		var user = users[i];
 		var name = user.name;
-		var stats= user.mods[0].stats;
+		if (user.mods.length === 0) {
+			return;
+		}
+		var stats = user.mods[0].stats;
 		if (undefined !== stats) {
 			userRecords[name] = stats;
 		}
-	}
+	});
 	var bookofrecords = {
-			ref: "text-bookofrecords",
-			pages: generateRecords(userRecords)
+		ref: "text-bookofrecords",
+		pages: generateRecords(userRecords),
 	}
-	File.writeFile(Argv.book, JSON.stringify(bookofrecords, null, 4), function(err) { } );
-
+	await File.writeFile(Argv.book, JSON.stringify(bookofrecords, null, 4));
 }
 
 const dbName = 'elko';
 
-MongoClient.connect("mongodb://" + Argv.mongo, function(err, client) {
-	Assert.equal(null, err);
+const generateBookOfRecords = async () => {
+	const client = await MongoClient.connect("mongodb://" + Argv.mongo);
 	let db = client.db(dbName);
-	var collection = db.collection('odb');
-	collection.find({"ref": {$regex: "user-*"}}).toArray(function(err, users) {
-		if (undefined !== users) {
-			processUserStats(users);
-		}
-		client.close();
-	});
-});
+	const users = await db.collection('odb').find({"ref": {$regex: "user-*"}});
+	const usersArray = await users.toArray();
+	if (undefined !== users) {
+		await processUserStats(users);
+	}
+	await client.close();
+}
 
-
-
-
-
+(async function main() {
+  await generateBookOfRecords();
+}());

.travis.yml
@@ -34,16 +34,4 @@ script:
   - sleep 60
 
   # Executes functional tests:
-  - cd test && node Telko.js -p 2018 -f chip
-
-after_success:
-  # Pushes the Docker image then deploys it to Spine.
-  - if [ "${TRAVIS_BRANCH}" == "master" ]; then
-      cd ${TRAVIS_BUILD_DIR};
-      docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}";
-      docker push ${DOCKER_USERNAME}/neohabitat;
-      if [[ "${SPINE_DEPLOY}" == true ]]; then
-        curl -o ./spinecli.tgz https://bin.equinox.io/c/jSZ5ZhbDXZ9/spine-cli-stable-linux-amd64.tgz && tar xvzf spinecli.tgz;
-        ./spine cluster deploy neohabitat;
-      fi
-    fi
+  - cd test && npm run telko -- -p 2018 -f chip

Backbone
@@ -1,96 +1,0 @@
----
-clusters:
-  - neohabitatmongo:
-      image: mongo
-      tag: "3.4.1"
-      volumes:
-        - mount_location: /data/db
-          size_gb: 10
-      ports:
-        - port: 27017
-
-  - neohabitatmariadb:
-      image: mariadb
-      environment:
-        - "MYSQL_ROOT_PASSWORD={{ vault.mysql_root_password }}"
-        - MYSQL_DATABASE=qlink
-        - MYSQL_USER=spine
-        - MYSQL_PASSWORD=spine
-      volumes:
-        - mount_location: /var/lib/mysql
-          size_gb: 10
-      ports:
-        - port: 3306
-
-  - neohabitat:
-      image: philcollins/neohabitat
-      environment:
-        - NEOHABITAT_DEFAULT_CONTEXT=context-Downtown_5f
-        - NEOHABITAT_BRIDGE_ELKO_HOST=127.0.0.1:2018
-        - NEOHABITAT_MONGO_HOST=neohabitatmongo:27017
-        - NEOHABITAT_SCHEMA_DIR=db
-        - NEOHABITAT_SHOULD_ENABLE_CRON=true
-        - NEOHABITAT_SHOULD_ENABLE_DEBUGGER=false
-        - NEOHABITAT_SHOULD_RUN_PUSHSERVER=true
-        - NEOHABITAT_SHOULD_UPDATE_SCHEMA=false
-        - NODE_ENV=production
-        - PUSH_SERVER_MONGO_URL=mongodb://neohabitatmongo
-      synapses:
-        - resource: neohabitatmongo
-          port: 27017
-      ports:
-        - port: 1337
-        - port: 1701
-        - port: 2018
-        - port: 9000
-
-  - neohabitatslackin:
-      image: chk1/slackin
-      environment:
-        - SLACK_ORG=neohabitat
-        - "SLACK_API_TOKEN={{ vault.neohabitat_slack_token }}"
-      ports:
-        - port: 3000
-
-  - neohabitatqlink:
-      image: philcollins/qlink
-      environment:
-        - QLINK_DB_HOST=neohabitatmariadb
-        - QLINK_DB_JDBC_URI=jdbc:mysql://neohabitatmariadb:3306/qlink
-        - QLINK_DB_USERNAME=spine
-        - QLINK_DB_PASSWORD=spine
-        - QLINK_HABITAT_HOST=neohabitat
-        - QLINK_SHOULD_CREATE_DB=true
-        - QLINK_SHOULD_PING=true
-      synapses:
-        - resource: neohabitat
-          port: 1337
-        - resource: neohabitatmariadb
-          port: 3306
-      ports:
-        - port: 1986
-        - port: 5190
-
-balancers:
-  - exhibit:
-      synapses:
-        - resource: neohabitat
-          protocol: http
-          remote_port: 1701
-
-  - neohabitat:
-      synapses:
-        - resource: neohabitat
-          port: 1337
-          remote_port: 2018
-        - resource: neohabitatqlink
-          port: 1986
-        - resource: neohabitatqlink
-          port: 5190
-
-  - join-neohabitat:
-      enable_ssl: false
-      synapses:
-        - resource: neohabitatslackin
-          protocol: http
-          remote_port: 3000

Dockerfile
@@ -2,29 +2,25 @@
 #
 # VERSION              0.1.0
 
-FROM quay.io/centos/centos:stream8
-
-# Installs MongoDB Yum repository.
-ADD https://goo.gl/CxNbGr /etc/yum.repos.d/mongodb-org.3.4.repo
+FROM quay.io/centos/centos:stream9
 
 # Get a recent version of nodejs
-RUN curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+RUN curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
 
 # Installs base build dependencies.
-RUN yum -y install \
-  cronie \
-  git \
-# htop \
-  java-1.8.0-openjdk \
-  make \
-  mariadb \
-  vim \
-  wget \
-  which \
-  maven \
-  mongodb-org \
-  nsolid  && \
-  yum clean all
+RUN dnf -y install \
+    cronie \
+    git \
+    java-21-openjdk \
+    make \
+    maven \
+    nc \
+    net-tools \
+    nsolid \
+    procps \
+    vim \
+    wget && \
+  dnf clean all
 
 # Installs Node dependencies.
 RUN npm install -g supervisor
@@ -41,10 +37,18 @@ RUN printf "*/5 * * * * root /bin/bash -c 'cd /neohabitat/db && NEOHABITAT_MONGO
 # Builds the Neohabitat project.
 WORKDIR /neohabitat
 RUN rm -rf lib && mvn clean package
-RUN npm ci
+
+WORKDIR /neohabitat/bridge
+RUN npm install
+
+WORKDIR /neohabitat/habibots
+RUN npm install
 
 WORKDIR /neohabitat/pushserver
-RUN npm ci
+RUN npm install
+
+WORKDIR /neohabitat/test
+RUN npm install
 
 WORKDIR /neohabitat
 ENTRYPOINT /neohabitat/run

Vagrantfile
@@ -1,36 +1,0 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-# If we're running on Windows, ensures that VBOX_INSTALL_PATH is appropriately set.
-if Gem.win_platform?
-  ENV["VBOX_INSTALL_PATH"] = "C:\\Program Files\\Oracle\\VirtualBox\\"
-end
-
-Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial64"
-  config.vm.box_check_update = false
-
-  config.vm.network "forwarded_port", guest: 1337, host: 1337, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 1701, host: 1701, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 1986, host: 1986, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 2018, host: 2018, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 3307, host: 3307, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 5190, host: 5190, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 9000, host: 9000, host_ip: "0.0.0.0"
-  config.vm.network "forwarded_port", guest: 9229, host: 9229, host_ip: "127.0.0.1"
-  config.vm.network "forwarded_port", guest: 27017, host: 27017, host_ip: "0.0.0.0"
-
-  config.vm.provider :virtualbox do |vb|
-    vb.customize ["modifyvm", :id, "--memory", "1024"]
-    vb.customize ["modifyvm", :id, "--cpus", "2"]
-  end
-
-  # Explicitly enforces syncing of the local folder to /vagrant on the VM.
-  config.vm.synced_folder ".", "/neohabitat"
-
-  if Gem.win_platform?
-    config.vm.provision :shell, binary: true, path: "./vagrant/build.sh"
-  else
-    config.vm.provision :shell, path: "./vagrant/build.sh"
-  end
-end

bootstrap
@@ -1,0 +1,136 @@
+#!/usr/bin/env bash
+# Installs all development dependencies.
+
+GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
+
+function ensure_mac_deps() {
+  echo " - Ensuring that Mac OS-style dependencies are installed (may ask for your root password)..."
+  type brew || {
+    echo " - Installing Brew (may ask for your sudo password)..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  }
+  type docker || {
+    echo " - Installing Docker via Brew..."
+    brew install docker docker-compose
+  }
+  type node || {
+    echo " - Installing node.js via Brew..."
+    brew install node
+  }
+  type mvn || {
+    echo " - Installing maven via Brew..."
+    brew install maven
+  }
+}
+
+function ensure_linux_deps_debian() {
+  echo " - Ensuring that Debian-style dependencies are installed (may ask for your sudo password)..."
+  sudo apt update -y
+  type docker || {
+    echo " - Installing Docker from official Docker repo..."
+    sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt update -y
+    sudo apt-cache policy docker-ce
+    sudo apt install docker-ce -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker "${USER}"
+  }
+  type mvn || {
+    echo " - Installing Maven..."
+    sudo apt install maven -y
+  }
+  type node || {
+    echo " - Installing Node.js..."
+    sudo apt install nodejs -y
+  }
+  type npm || {
+    echo " - Installing NPM..."
+    sudo apt install npm -y
+  }
+}
+
+function ensure_linux_deps_redhat() {
+  echo " - Ensuring that Red Hat-style dependencies are installed (may ask for your sudo password)..."
+  docker_repo="fedora"
+  if [[ "${ID}" =~ (centos|rhel) ]]; then
+    echo " - Ensuring that EPEL is installed..."
+    docker_repo="rhel"
+    el_version=$(cat /etc/system-release-cpe | awk -F: '{ print $5 }' | grep -o ^[0-9]*)
+    if [ "${ID}" == "rhel" ]; then
+      sudo subscription-manager repos --enable "codeready-builder-for-rhel-${el_version}-$(arch)-rpms"
+      sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${el_version}.noarch.rpm"
+    else
+      sudo dnf config-manager --set-enabled crb
+      sudo dnf install -y \
+          "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${el_version}.noarch.rpm" \
+          "https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-${el_version}.noarch.rpm"
+    fi
+  fi
+  type docker-compose || {
+    echo " - Installing Docker from official Docker repo..."
+    sudo dnf install dnf-plugins-core -y
+    sudo dnf config-manager --add-repo "https://download.docker.com/linux/${docker_repo}/docker-ce.repo"
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker "${USER}"
+  }
+  type mvn || {
+    echo " - Installing Maven..."
+    sudo dnf install maven -y
+  }
+  type node || {
+    echo " - Installing Node.js..."
+    sudo dnf install nodejs -y
+  }
+}
+
+function ensure_linux_deps() {
+  source /etc/os-release
+  if [[ "${ID}" =~ (debian|ubuntu) ]]; then
+    ensure_linux_deps_debian
+  elif [[ "${ID}" =~ (rhel|fedora) ]]; then
+    ensure_linux_deps_redhat
+  else
+    echo " ! Unknown Linux distribution (${ID}); unable to preload dependencies."
+  fi
+}
+
+case "$(uname -sr)" in
+  Darwin*)
+    ensure_mac_deps
+    ;;
+  Linux*)
+    ensure_linux_deps
+    ;;
+  *)
+    echo " ! ERROR: You must run this script on either a Linux, WSL or Mac OS environment"
+    exit 1
+    ;;
+esac
+
+echo " - Ensuring that all Node.js dependencies are installed..."
+
+cd "${GIT_BASE_DIR}/bridge"
+npm install
+
+cd "${GIT_BASE_DIR}/db"
+npm install
+
+cd "${GIT_BASE_DIR}/pushserver"
+npm install
+
+cd "${GIT_BASE_DIR}/habibots"
+npm install
+
+cd "${GIT_BASE_DIR}/test"
+npm install
+
+echo " * Bootstrapping complete!"
+echo " * Run the following command to bring up all Neohabitat services for the first time..."
+echo " $ ./recreate -b"
+echo " * Subsequent runs of the Neohabitat services should be brought up with:"
+echo " $ docker compose up"

commit 7a030efbdce129ee45fc1b93cb7def353c4fafbe

db: fix generateBookOfRecords broken since Aug 2024
Two bugs introduced in 1bd92fc7:
1. processUserStats() was called with the cursor instead of usersArray
2. forEach callback shadowed the user parameter with users[i] where i
   is undefined, silently producing no records

Hall of Fame has been empty for ~9 months as a result.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

db/generateBookOfRecords.js
@@ -196,7 +196,6 @@ function generateRecords(userRecords) {
 const processUserStats = async (users) => {
 	var userRecords = {};
 	users.forEach((user) => {
-		var user = users[i];
 		var name = user.name;
 		if (user.mods.length === 0) {
 			return;
@@ -220,8 +219,8 @@ const generateBookOfRecords = async () => {
 	let db = client.db(dbName);
 	const users = await db.collection('odb').find({"ref": {$regex: "user-*"}});
 	const usersArray = await users.toArray();
-	if (undefined !== users) {
-		await processUserStats(users);
+	if (undefined !== usersArray) {
+		await processUserStats(usersArray);
 	}
 	await client.close();
 }